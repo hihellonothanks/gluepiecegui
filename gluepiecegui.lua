84bbedc7f982c91a7ac7f0339313ca9e9c33c139
hihellonothanks <77178317+hihellonothanks@users.noreply.github.com>
2021-05-04 15:37:02 -0600

    Update gluepiecegui.lua

--- gluepiecegui.lua
+++ gluepiecegui.lua
@@ -1,4 +1,4 @@
--- https ://xheptcofficial.gitbook.io/kavo-library/
+-- https://xheptcofficial.gitbook.io/kavo-library/
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
@@ -103,7 +103,10 @@ Section:NewDropdown("Select Target", "DropdownInf", {
     target = currentOption
 end)
 
-
+_G.distance = nil
+Section:NewSlider("Distance", "SliderInfo", 20, -20, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    _G.distance = s
+end)
 
 _G.tping = nil
 Section:NewToggle("Autofarm Target", "ToggleInfo", function(state2)
@@ -112,16 +115,32 @@ if state2 then
     else
     _G.tping = false
 end
-    while _G.tping do wait(0.5)
+    while _G.tping do wait()
+
+           game:GetService("RunService").RenderStepped:Connect(function()
+               if _G.tping then
+    Plr.Character.Humanoid:ChangeState(11)
+    end
+           end)
+
+if _G.distance < 0 then
+game.Players.LocalPlayer.DevCameraOcclusionMode = "Invisicam"
+else
+game.Players.LocalPlayer.DevCameraOcclusionMode = "Zoom"
+end
+
+        wait(0.5)
     for _,v in pairs(game.Workspace:GetDescendants()) do
   if string.find(v.Name, target) and v:FindFirstChild("HumanoidRootPart") ~= nil then
       if v:FindFirstChild("Humanoid").Health > 0 then
-      game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = v:WaitForChild("HumanoidRootPart").CFrame*CFrame.new(0, 0, 7)
+      game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = v:WaitForChild("HumanoidRootPart").CFrame*CFrame.new(0, _G.distance, 0)
+      Plr.Character.HumanoidRootPart.CFrame = CFrame.new(Plr.Character.HumanoidRootPart.Position, v.HumanoidRootPart.Position)
       end
   end
     end
     end
 end)
+
 
 local Section = Tab:NewSection("Other")
 
