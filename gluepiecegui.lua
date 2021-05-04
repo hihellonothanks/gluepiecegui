b6826a96abe623d4bb7654178b3f105348a65f0c
hihellonothanks <77178317+hihellonothanks@users.noreply.github.com>
2021-05-03 23:37:45 -0600

    Update gluepiecegui.lua

--- gluepiecegui.lua
+++ gluepiecegui.lua
@@ -1,6 +1,6 @@
 -- https ://xheptcofficial.gitbook.io/kavo-library/
 
-local Library = loadstring(game:HttpGet("https ://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
 local Window = Library.CreateLib("Glue Piece", "DarkTheme")
 
